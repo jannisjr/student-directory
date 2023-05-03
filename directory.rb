7d7ebc541d0c7e78d83e3af2d21f7d61fdbcc1f3
Joe Hannis <joe_hannis@hotmail.com>
2023-05-03 12:35:43 +0100

    created student csv file to stort info. Added git ignore for csv file

--- directory.rb
+++ directory.rb
@@ -1,4 +1,13 @@
 @students = []
+def save_students
+  file = file.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [[student[:name]], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 def input_students
   puts "Please enter the name of the student"
@@ -43,6 +52,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  PUTS "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -58,6 +68,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
