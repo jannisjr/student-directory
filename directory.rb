bbabcb5587a78932704b1b55cd5b32883fff2694
Joe Hannis <joe_hannis@hotmail.com>
2023-05-03 13:41:36 +0100

    created try_load_students method to be able to pass arguments from the cmd line

--- directory.rb
+++ directory.rb
@@ -1,62 +1,4 @@
 @students = []
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [[student[:name]], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def input_students
-  puts "Please enter the name of the student"
-  puts "To finish, just hit return twice"
-  name = gets.delete("\n").capitalize
-  while !name.empty? do
-    puts "Please enter the student's country of birth"
-    country = gets.chomp.capitalize
-    puts "Please enter the student's hobbies"
-    hobbies = gets.chomp
-    puts "Please enter the student's height in metres and centimetres"
-    height = gets.chomp
-    puts "What cohort month are they in?"
-    month = gets.chomp.capitalize
-    if month.empty?
-      month = "November"
-    else
-      until spelling(month) == true
-        puts "Please check the spelling of the month and try again"
-        month = gets.chomp.capitalize
-      end
-    end
-    @students << {name: name, country: country, hobbies: hobbies, height: height, cohort: month.to_sym}
-    if @students.count == 1
-      puts "Now we have #{@students.count} student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
-    puts "Please enter the name of the student"
-    puts "To finish, just hit return twice"
-    name = gets.chomp.capitalize
-  end
-end
-
-def interactive_menu
-  loop do
-   print_menu
-   process(gets.chomp)
-  end
-end
 
 def print_menu
   puts "1. Input the students"
@@ -66,10 +8,11 @@ def print_menu
   puts "9. Exit"
 end
 
-def show_students
-  print_header
-  print_student_list
-  print_footer
+def interactive_menu
+  loop do
+   print_menu
+   process(STDIN.gets.chomp)
+  end
 end
 
 def process(selection)
@@ -89,22 +32,43 @@ def process(selection)
   end
 end
 
-def spelling(month)
-  month_array = [ 
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ]
-  month_array.include?(month)
+def input_students
+  puts "Please enter the name of the student"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.delete("\n").capitalize
+  while !name.empty? do
+    puts "Please enter the student's country of birth"
+    country = STDIN.gets.chomp.capitalize
+    puts "Please enter the student's hobbies"
+    hobbies = STDIN.gets.chomp
+    puts "Please enter the student's height in metres and centimetres"
+    height = STDIN.gets.chomp
+    puts "What cohort month are they in?"
+    month = STDIN.gets.chomp.capitalize
+    if month.empty?
+      month = "November"
+    else
+      until spelling(month) == true
+        puts "Please check the spelling of the month and try again"
+        month = STDIN.gets.chomp.capitalize
+      end
+    end
+    @students << {name: name, country: country, hobbies: hobbies, height: height, cohort: month.to_sym}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
+    else
+      puts "Now we have #{@students.count} students"
+    end
+    puts "Please enter the name of the student"
+    puts "To finish, just hit return twice"
+    name = STDIN.gets.chomp.capitalize
+  end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -168,4 +132,56 @@ def group_by_cohort
   end
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first# first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
+def spelling(month)
+  month_array = [ 
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+  month_array.include?(month)
+end
+
+try_load_students
 interactive_menu
